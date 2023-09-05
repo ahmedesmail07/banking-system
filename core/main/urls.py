3c3d04823da7030cf58d03922fb5cf164b2616a5
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-09-05 19:30:23 +0300

    transfer-money-part2

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 from .transfer import *
+
 app_name = "main"
 
 urlpatterns = [
@@ -8,7 +9,5 @@ urlpatterns = [
     path("contact/", contact, name="contact"),
     path("about/", about, name="about"),
     path("search-account/", searchAccount, name="search-account"),
-    path("transfer-amount/<account_number>/",
-         transferAmount, name="transfer-amount"),
-
+    path("transfer-amount/<account_number>/", transferAmount, name="transfer-amount"),
 ]
